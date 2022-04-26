82916a1f70efecad9d82bf542e9f5ba3c0c92397
Adeel Bukhari <adeelbukhari@gmail.com>
2022-04-25 20:33:50 -0400

    Add kata

--- 8kyu/Pillars.js
+++ 8kyu/Pillars.js
@@ -5,20 +5,19 @@
     width of the pillar (10 - 50 centimeters).
 
 Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
-
- */
+*/
 
 function pillars(num_pill, dist, width) {
-    if(num_pill < 2){
-      return 0
-    }else if (num_pill < 3){
-      return dist * 100
-    }else{
-      return ((num_pill - 2)*width)+((num_pill -1) * (dist * 100))
-    }
+  if (num_pill < 2) {
+    return 0;
+  } else if (num_pill < 3) {
+    return dist * 100;
+  } else {
+    return (num_pill - 2) * width + (num_pill - 1) * (dist * 100);
   }
+}
 
-    console.log(pillars(1, 1, 1), 0);
-    console.log(pillars(2, 1, 1), 100);
-    console.log(pillars(2, 20, 25), 2000);
-    console.log(pillars(11, 15, 30), 15270);
+console.log(pillars(1, 1, 1), 0);
+console.log(pillars(2, 1, 1), 100);
+console.log(pillars(2, 20, 25), 2000);
+console.log(pillars(11, 15, 30), 15270);
