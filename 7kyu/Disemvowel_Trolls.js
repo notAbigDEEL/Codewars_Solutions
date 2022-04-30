c948675ca373eafb84c734c00f84ee3a242feb94
Adeel Bukhari <adeelbukhari@gmail.com>
2022-04-30 15:00:44 -0400

    Add kata

---
+++ 7kyu/Disemvowel_Trolls.js
@@ -0,0 +1,30 @@
+/* Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+ */
+
+function disemvowel(str) {
+    let vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+    let result = ''
+  
+    
+    for (let i = 0; i < str.length; i++){
+      if (vowels.includes(str[i])){
+        continue
+      }else{
+        result += str[i]
+      }
+    }
+    return result
+  }
+
+
+console.log(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!");
+console.log(disemvowel("That car is cool"), "Tht cr s cl!");
--- 7kyu/Vowel_Count.js
+++ 7kyu/Vowel_Count.js
@@ -5,3 +5,20 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 
 */
+
+function getCount(str) {
+  var vowelsCount = 0;
+  str = str.toString();
+  let vowels = ["a", "e", "i", "o", "u"];
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      vowelsCount++;
+    }
+  }
+  return vowelsCount;
+}
+
+console.log(getCount("abracadabra"), 5);
+console.log(getCount(""), 0);
+console.log(getCount("abthdfe"), 2);
