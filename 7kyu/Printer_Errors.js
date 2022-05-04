9db739a2db9c0388cfc29cb0219908f0377609fe
Adeel Bukhari <adeelbukhari@gmail.com>
2022-05-04 14:22:10 -0400

    Add kata

---
+++ 7kyu/Printer_Errors.js
@@ -0,0 +1,41 @@
+/*In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only letters from ato z.
+Examples:
+
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22"
+
+ */
+
+function printerError(s) {
+  let sArr = s.split("");
+  let errors = 0;
+  let dict = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+
+  for (let i = 0; i < sArr.length; i++) {
+    if (dict.includes(sArr[i])) {
+      errors += 0;
+    } else {
+      errors += 1;
+    }
+  }
+
+  return `${errors}/${s.length}`;
+}
+
+console.log(printerError("aaabbbbhaijjjm"), "0/14");
+console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"), "8/22");
+console.log(
+  printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"),
+  "3/56"
+);
