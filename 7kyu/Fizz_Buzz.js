e569298b1b7eef987ab178ebe7e4d5f5cd39dca1
Adeel Bukhari <adeelbukhari@gmail.com>
2022-05-07 00:38:54 -0400

    Add kata

---
+++ 7kyu/Fizz_Buzz.js
@@ -0,0 +1,54 @@
+/* Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+    If the value is a multiple of 3: use the value "Fizz" instead
+    If the value is a multiple of 5: use the value "Buzz" instead
+    If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+ */
+
+// Return an array
+function fizzbuzz(n) {
+  let result = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+console.log(fizzbuzz(15), [
+  1,
+  2,
+  "Fizz",
+  4,
+  "Buzz",
+  "Fizz",
+  7,
+  8,
+  "Fizz",
+  "Buzz",
+  11,
+  "Fizz",
+  13,
+  14,
+  "FizzBuzz",
+]);
+console.log(fizzbuzz(3), [1, 2, "Fizz"]);
+console.log(fizzbuzz(5), [1, 2, "Fizz", 4, "Buzz"]);
