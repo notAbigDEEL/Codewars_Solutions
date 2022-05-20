0e3468d0a499afa998482e01318118037948d0d8
Adeel Bukhari <adeelbukhari@gmail.com>
2022-05-20 18:03:12 -0400

    Add kata

--- 8kyu/Power.js
+++ 8kyu/Power.js
@@ -4,17 +4,16 @@ Examples
 numberToPower(3,2)  // -> 9 ( = 3 * 3 )
 numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
 numberToPower(10,6) // -> 1000000
-
 */
 
-function numberToPower(number, power){
-    let result = 1
-    for(let i = 1 ; i <=power; i++){
-      result *= number 
-    }  
-    return result
+function numberToPower(number, power) {
+  let result = 1;
+  for (let i = 1; i <= power; i++) {
+    result *= number;
   }
+  return result;
+}
 
-    console.log(numberToPower(3,2), 9)
-    console.log(numberToPower(2,3), 8)
-    console.log(numberToPower(10,6), 1000000)
+console.log(numberToPower(3, 2), 9);
+console.log(numberToPower(2, 3), 8);
+console.log(numberToPower(10, 6), 1000000);
