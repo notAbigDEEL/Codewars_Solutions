e913aaf0104596a47bafc7cdd2a84accbf73bf5d
Adeel Bukhari <adeelbukhari@gmail.com>
2022-05-26 23:13:50 -0400

    Add kata

--- 8kyu/Grasshopper_Grade_Book.js
+++ 8kyu/Grasshopper_Grade_Book.js
@@ -12,17 +12,19 @@ Tested values are all between 0 and 100. Theres is no need to check for negative
 
  */
 
-function getGrade (s1, s2, s3) {
-    let average = (s1 + s2 + s3) / 3
-    if (average >= 90){
-      return 'A'
-    }else if (average >= 80){
-      return 'B'
-    }else if (average >= 70){
-      return 'C'
-    }else if (average >= 60){
-      return 'D'
-    }else{
-      return 'F'
-    }
+function getGrade(s1, s2, s3) {
+  let average = (s1 + s2 + s3) / 3;
+  if (average >= 90) {
+    return "A";
+  } else if (average >= 80) {
+    return "B";
+  } else if (average >= 70) {
+    return "C";
+  } else if (average >= 60) {
+    return "D";
+  } else {
+    return "F";
   }
+}
+
+console.log(getGrade(86, 92, 88), "B");
