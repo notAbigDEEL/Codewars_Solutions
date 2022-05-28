5170898cd5a0acea32e6c44950448c635e1863b4
Adeel Bukhari <adeelbukhari@gmail.com>
2022-05-28 19:59:03 -0400

    Add Kata

--- 8kyu/Name_Shuffler.js
+++ 8kyu/Name_Shuffler.js
@@ -1,5 +1,4 @@
-/*
-Write a function that returns a string in which firstname is swapped with last name.
+/*Write a function that returns a string in which firstname is swapped with last name.
 
 nameShuffler('john McClane'); => "McClane john"
 
