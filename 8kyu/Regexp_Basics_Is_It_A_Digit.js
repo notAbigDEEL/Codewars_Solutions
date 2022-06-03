e8d85a97bf132d108cc15a49e2dc8a2e9fd1e02f
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-03 19:59:29 -0400

    Add Kata

--- 8kyu/Regexp_Basics_Is_It_A_Digit.js
+++ 8kyu/Regexp_Basics_Is_It_A_Digit.js
@@ -1,4 +1,5 @@
-/*Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+/*
+Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
 
  */
 
@@ -6,6 +7,6 @@ String.prototype.digit = function () {
   return /^\d$/.test(this);
 };
 
-console.log('1'.digit(), true);
-console.log('a'.digit(), false);
-console.log('0'.digit(), true);
+console.log("1".digit(), true);
+console.log("a".digit(), false);
+console.log("0".digit(), true);
