ff8df5f2397679e2d5e1fd766a8055e910e099b1
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-05 19:52:17 -0400

    Add Kata

--- 8kyu/Find_Max_Min_Values_Of_List.js
+++ 8kyu/Find_Max_Min_Values_Of_List.js
@@ -1,5 +1,4 @@
-/*
-Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+/* Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
 Examples (Input -> Output)
 
 * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
