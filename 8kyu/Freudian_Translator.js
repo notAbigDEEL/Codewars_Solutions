99f3b6857d761f4501d1cf7eb79ac8947342607e
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-07 19:51:26 -0400

    Add Kata

--- 8kyu/Freudian_Translator.js
+++ 8kyu/Freudian_Translator.js
@@ -1,5 +1,4 @@
-/* 
-You probably know that number 42 is "the answer to life, the universe and everything" according to Douglas Adams' "The Hitchhiker's Guide to the Galaxy". For Freud, the answer was quite different...
+/* You probably know that number 42 is "the answer to life, the universe and everything" according to Douglas Adams' "The Hitchhiker's Guide to the Galaxy". For Freud, the answer was quite different...
 
 In the society he lived in, people - women in particular - had to repress their sexual needs and desires. This was simply how the society was at the time. Freud then wanted to study the illnesses created by this, and so he digged to the root of their desires. This led to some of the most important psychoanalytic theories to this day, Freud being the father of psychoanalysis.
 
