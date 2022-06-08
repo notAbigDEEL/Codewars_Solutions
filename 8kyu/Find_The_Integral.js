894970c9d5f99cd3b4456d3abb5b2a9ee3577e42
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-08 19:49:59 -0400

    Add Kata

--- 8kyu/Find_The_Integral.js
+++ 8kyu/Find_The_Integral.js
@@ -14,18 +14,16 @@ Examples
 20, 1  -->  "10x^2"
 40, 3  -->  "10x^4"
 90, 2  -->  "30x^3"
-
- */
+*/
 
 function integrate(coefficient, exponent) {
-    let exp = exponent + 1
-    let den = coefficient / (exponent + 1)
-    return `${den}x^${exp}`
-    
-  }
+  let exp = exponent + 1;
+  let den = coefficient / (exponent + 1);
+  return `${den}x^${exp}`;
+}
 
-    console.log(integrate(3, 2), "1x^3")
-    console.log(integrate(12, 5), "2x^6")
-    console.log(integrate(20, 1), "10x^2")
-    console.log(integrate(40, 3), "10x^4")
-    console.log(integrate(90, 2), "30x^3")
+console.log(integrate(3, 2), "1x^3");
+console.log(integrate(12, 5), "2x^6");
+console.log(integrate(20, 1), "10x^2");
+console.log(integrate(40, 3), "10x^4");
+console.log(integrate(90, 2), "30x^3");
