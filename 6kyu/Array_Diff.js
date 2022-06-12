632b7cdf70b9c3975533d61633c6124487ace53e
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-11 21:49:53 -0400

    Add Kata

--- 6kyu/Array_Diff.js
+++ 6kyu/Array_Diff.js
@@ -7,6 +7,7 @@ arrayDiff([1,2],[1]) == [2]
 If a value is present in b, all of its occurrences must be removed from the other:
 
 arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
 */
 
 function arrayDiff(a, b) {
