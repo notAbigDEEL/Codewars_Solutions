161566e88eaa7b5eb11345a4d950934478960924
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-13 21:16:21 -0400

    Add Kata

--- 6kyu/Count_Characters_In_Your_String.js
+++ 6kyu/Count_Characters_In_Your_String.js
@@ -2,26 +2,24 @@
 The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 
 What if the string is empty? Then the result should be empty object literal, {}.
+*/
 
- */
+function count(string) {
+  let result = {};
 
-function count (string) {  
-
-    let result = {}
-    
-    if(!string){
-      return {}
-    }
-  
-    for (const ch of string){
-      if(result.hasOwnProperty(ch)){
-        result[ch] += 1
-      }else{
-        result[ch] = 1
-      }
-    }
-    return result
+  if (!string) {
+    return {};
   }
 
-    console.log(count("aba"), {a: 2, b: 1});
-    console.log(count("caabbu"), {a: 2, b: 2, c: 1, u: 1});
+  for (const ch of string) {
+    if (result.hasOwnProperty(ch)) {
+      result[ch] += 1;
+    } else {
+      result[ch] = 1;
+    }
+  }
+  return result;
+}
+
+console.log(count("aba"), { a: 2, b: 1 });
+console.log(count("caabbu"), { a: 2, b: 2, c: 1, u: 1 });
