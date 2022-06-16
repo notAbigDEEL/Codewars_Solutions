7baaf75b8d8b7da22b55eef1eea080e4979295c7
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-15 23:38:37 -0400

    Add Kata

--- 6kyu/Split_Strings.js
+++ 6kyu/Split_Strings.js
@@ -1,4 +1,5 @@
-/*Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+/*
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
 Examples:
 
@@ -7,24 +8,24 @@ Examples:
 
  */
 
-function solution(str){
-    let length = str.length
-    let result = []
-    
-    for(let i = 0; i < str.length; i+=2){
-      result.push(str.slice(i, i+2))
-    }
-    if(str.length % 2 === 0){
-      return result
-    }else{
-      let special = result[result.length-1]
-      result.pop()
-      result.push(special+"_")
-    }
-    
-    return result
+function solution(str) {
+  let length = str.length;
+  let result = [];
+
+  for (let i = 0; i < str.length; i += 2) {
+    result.push(str.slice(i, i + 2));
+  }
+  if (str.length % 2 === 0) {
+    return result;
+  } else {
+    let special = result[result.length - 1];
+    result.pop();
+    result.push(special + "_");
   }
 
-    console.log(solution("abc"), ["ab", "c_"]);
-    console.log(solution("abcdef"), ["ab", "cd", "ef"]);
-    console.log(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+  return result;
+}
+
+console.log(solution("abc"), ["ab", "c_"]);
+console.log(solution("abcdef"), ["ab", "cd", "ef"]);
+console.log(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
