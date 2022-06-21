dc18c1b07ae8dc5f67a2f273600c1bb9fbe8770f
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-21 13:14:28 -0400

    Add Kata

--- 6kyu/Which_Are_In.js
+++ 6kyu/Which_Are_In.js
@@ -1,4 +1,5 @@
-/* Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+/* 
+Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
 Example 1:
 
 a1 = ["arp", "live", "strong"]
