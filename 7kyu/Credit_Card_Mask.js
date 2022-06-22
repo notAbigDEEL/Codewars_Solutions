b23afe8c53d0e84bdcededf84fa499f696f7d36b
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-22 00:22:36 -0400

    Add Kata

--- 7kyu/Credit_Card_Mask.js
+++ 7kyu/Credit_Card_Mask.js
@@ -1,4 +1,5 @@
-/*screen. Instead, we mask it.
+/*
+screen. Instead, we mask it.
 
 Your task is to write a function maskify, which changes all but the last four characters into '#'.
 Examples
@@ -19,15 +20,15 @@ Examples
  */
 
 function maskify(cc) {
-    let ccArr = [];
-    for (let i = 0; i < cc.length - 4; i++) {
-      ccArr.push("#");
-    }
-    for (let i = cc.length - 4; i < cc.length; i++) {
-      ccArr.push(cc[i]);
-    }
-    return ccArr.join("");
+  let ccArr = [];
+  for (let i = 0; i < cc.length - 4; i++) {
+    ccArr.push("#");
   }
+  for (let i = cc.length - 4; i < cc.length; i++) {
+    ccArr.push(cc[i]);
+  }
+  return ccArr.join("");
+}
 
 console.log(maskify("4556364607935616"), "############5616");
 console.log(maskify("64607935616"), "#######5616");
