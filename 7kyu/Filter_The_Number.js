6f048abfa91074a8b01cb53758c2a6e0006f3773
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-27 15:00:26 -0400

    Add Kata

--- 7kyu/Filter_The_Number.js
+++ 7kyu/Filter_The_Number.js
@@ -1,4 +1,5 @@
-/*Filter the number
+/*
+Filter the number
 
 Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
 Task
