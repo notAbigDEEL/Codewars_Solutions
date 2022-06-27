a5a38ca81e0166141e37798c2f2d4b270db92952
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-27 15:01:07 -0400

    Add Kata

--- 7kyu/Find_Middle_Element.js
+++ 7kyu/Find_Middle_Element.js
@@ -1,4 +1,5 @@
-/* As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+/* 
+As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
 
 The input to the function will be an array of three distinct numbers (Haskell: a tuple).
 
