d78ceaf88b8bae4cf175175c4f0c37daa7176a8d
Adeel Bukhari <adeelbukhari@gmail.com>
2022-06-29 22:31:16 -0400

    Add Kata

--- 7kyu/Breaking_Chocolate_Problem.js
+++ 7kyu/Breaking_Chocolate_Problem.js
@@ -1,4 +1,4 @@
-/*Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+/* Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
 
 For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
 
@@ -6,15 +6,15 @@ If input data is invalid you should return 0 (as in no breaks are needed if we d
 
  */
 
-function breakChocolate(n,m) {
-    if(n===0 || m === 0){
-      return 0
-    }else{
-      return n*m - 1
-    }
+function breakChocolate(n, m) {
+  if (n === 0 || m === 0) {
+    return 0;
+  } else {
+    return n * m - 1;
   }
+}
 
-    console.log(breakChocolate(5,5), 24);
-    console.log(breakChocolate(1,1), 0);
-    console.log(breakChocolate(2,1), 1);
-    console.log(breakChocolate(3,1), 2);
+console.log(breakChocolate(5, 5), 24);
+console.log(breakChocolate(1, 1), 0);
+console.log(breakChocolate(2, 1), 1);
+console.log(breakChocolate(3, 1), 2);
