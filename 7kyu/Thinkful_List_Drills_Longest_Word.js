f3d461c30fbdb56e1e490c32b0f98830fc827acd
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-12 18:37:04 -0400

    Add Kata

--- 7kyu/Thinkful_List_Drills_Longest_Word.js
+++ 7kyu/Thinkful_List_Drills_Longest_Word.js
@@ -1,4 +1,5 @@
-/*  Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+/* 
+ Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
 
 For example:
 
