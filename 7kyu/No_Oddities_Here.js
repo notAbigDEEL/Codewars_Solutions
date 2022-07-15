4d1028a77f9987dcdb2ce7e1a9a9ecbe758834f1
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-15 02:19:53 -0400

    Add Kata

--- 7kyu/No_Oddities_Here.js
+++ 7kyu/No_Oddities_Here.js
@@ -1,4 +1,5 @@
-/* Write a small function that returns the values of an array that are not odd.
+/* 
+Write a small function that returns the values of an array that are not odd.
 
 All values in the array will be integers. Return the good values in the order they are given.
 
