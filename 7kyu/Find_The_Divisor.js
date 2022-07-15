92d5f63c01eedc09f4b92a6fd14075d271c63db1
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-15 01:07:53 -0400

    Add Kata

--- 7kyu/Find_The_Divisor.js
+++ 7kyu/Find_The_Divisor.js
@@ -1,4 +1,5 @@
 /*
+
 Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
 Example:
 
@@ -9,19 +10,19 @@ divisors(13); // should return "13 is prime"
  */
 
 function divisors(integer) {
-    let result = []
-    for(let i = 2; i < integer; i++){
-      if(integer % i === 0){
-        result.push(i)
-      }
+  let result = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) {
+      result.push(i);
     }
-    if(result.length === 0){
-      return `${integer} is prime`
-    }else{
-      return result
-    }
-  };
+  }
+  if (result.length === 0) {
+    return `${integer} is prime`;
+  } else {
+    return result;
+  }
+}
 
-    console.log(divisors(12), [2,3,4,6]);
-    console.log(divisors(25), [5]);
-    console.log(divisors(13), "13 is prime");
+console.log(divisors(12), [2, 3, 4, 6]);
+console.log(divisors(25), [5]);
+console.log(divisors(13), "13 is prime");
