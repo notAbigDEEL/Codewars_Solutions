7f66920de4d06f9bdf488e81ff531fbc7615abfc
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-17 04:33:09 -0400

    Add Kata

--- 7kyu/Remove_The_Minimum.js
+++ 7kyu/Remove_The_Minimum.js
@@ -1,5 +1,4 @@
 /* 
-
 The museum of incredible dull things
 
 The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
