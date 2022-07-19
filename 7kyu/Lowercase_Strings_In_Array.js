8d17b113d2ccff49febf7242c0a77a35d371e80a
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-18 21:31:43 -0400

    Add Kata

--- 7kyu/Lowercase_Strings_In_Array.js
+++ 7kyu/Lowercase_Strings_In_Array.js
@@ -1,4 +1,5 @@
-/* Create a function to lowercase all strings in an array. Non-string items should remain unchanged.
+/* 
+Create a function to lowercase all strings in an array. Non-string items should remain unchanged.
 Example
 
 arrayLowerCase(['Red', 'Green']) == ['red', 'green']
@@ -7,18 +8,17 @@ arrayLowerCase([1, 'Green']) == [1, 'green']
  */
 
 function arrayLowerCase(arr) {
-
-    let result = []
-    for(let i = 0; i < arr.length; i++){
-      if(typeof arr[i] === "string"){
-        result.push(arr[i].toLowerCase())
-      }else{
-        result.push(arr[i])
-      }
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      result.push(arr[i].toLowerCase());
+    } else {
+      result.push(arr[i]);
     }
-    
-    return result
   }
 
-    console.log(arrayLowerCase(['Red', 'Green']), ['red', 'green']);
-    console.log(arrayLowerCase([1, 'Green']), [1, 'green']);
+  return result;
+}
+
+console.log(arrayLowerCase(["Red", "Green"]), ["red", "green"]);
+console.log(arrayLowerCase([1, "Green"]), [1, "green"]);
