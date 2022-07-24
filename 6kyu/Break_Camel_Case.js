cce84a59a165f831db6b5414df51a5a87ea3c36a
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-23 22:27:56 -0400

    Add Kata

--- 6kyu/Break_Camel_Case.js
+++ 6kyu/Break_Camel_Case.js
@@ -1,4 +1,5 @@
-/* Complete the solution so that the function will break up camel casing, using a space between words.
+/* 
+Complete the solution so that the function will break up camel casing, using a space between words.
 Example
 
 "camelCasing"  =>  "camel Casing"
