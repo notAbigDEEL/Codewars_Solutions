fe4256871506d0644c5c9d02ebd3e7486ef86414
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-25 14:31:43 -0400

    Add Kata

--- 6kyu/Is_A_Number_Prime.js
+++ 6kyu/Is_A_Number_Prime.js
@@ -1,5 +1,5 @@
 /* 
-Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+ Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
 
 Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 Requirements
