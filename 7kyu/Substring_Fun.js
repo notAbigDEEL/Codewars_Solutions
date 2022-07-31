fb2cc77f07dcdbfecc279e7c6f2c1920086f56c6
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-31 18:48:43 -0400

    Add Kata

--- 7kyu/Substring_Fun.js
+++ 7kyu/Substring_Fun.js
@@ -1,4 +1,5 @@
 /*
+
 Complete the function that takes an array of words.
 
 You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
