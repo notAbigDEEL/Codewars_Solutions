5305808e1808f502d762e51bfd2c23d17f2dc37d
Adeel Bukhari <adeelbukhari@gmail.com>
2022-07-31 18:47:46 -0400

    Add Kata

--- 7kyu/String_Merge.js
+++ 7kyu/String_Merge.js
@@ -1,4 +1,5 @@
-/* Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+/* 
+Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
 Examples
 
 ("hello", "world", "l")       ==>  "held"
@@ -8,13 +9,13 @@ Examples
 
  */
 
-function stringMerge(string1, string2, letter){
-    let part1 = string1.slice(0, string1.indexOf(letter))
-    let part2 = string2.slice(string2.indexOf(letter))
-    return part1 + part2
-  }
+function stringMerge(string1, string2, letter) {
+  let part1 = string1.slice(0, string1.indexOf(letter));
+  let part2 = string2.slice(string2.indexOf(letter));
+  return part1 + part2;
+}
 
-    console.log(stringMerge("hello", "world", "l"), "held")
-    console.log(stringMerge("coding", "anywhere", "n"), "codinywhere")
-    console.log(stringMerge("jason", "samson", "s"), "jasamson")
-    console.log(stringMerge("wonderful", "people", "e"), "wondeople")
+console.log(stringMerge("hello", "world", "l"), "held");
+console.log(stringMerge("coding", "anywhere", "n"), "codinywhere");
+console.log(stringMerge("jason", "samson", "s"), "jasamson");
+console.log(stringMerge("wonderful", "people", "e"), "wondeople");
